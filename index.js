8c16998ddcb6e6592ec7b33046603f55ee77ada6
DAbrenica22 <dilen_abrenica@dlsu.edu.ph>
2024-07-10 20:19:36 +0800

    design showing but not pictures

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const express = require('express');
-const { engine } = require('express-handlebars');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db'); // Adjust path to db.js in src/config
 const profileRoutes = require('./src/routes/profile'); // Adjust path to profile.js in src/routes
 const foodsRouter = require('./src/routes/foods');
 const path = require('path');
-
 const app = express();
-
+const hbs = require('hbs')
+const exhbs = require('express-handlebars')
+const templatePath = path.join(__dirname, "./src/views")
 // Connect to MongoDB
 connectDB();
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 // Handlebars
-app.engine('hbs', engine({ extname: '.hbs', defaultLayout: 'main', layoutsDir: path.join(__dirname, '/src/views/') }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, '/src/views')); // Adjust views directory path
+app.set('views', templatePath); // Adjust views directory path
 
 // Authentication middleware for demo purposes
 app.use((req, res, next) => {
